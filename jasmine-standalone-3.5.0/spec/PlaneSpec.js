836061b3e02a5c06d5bba8502cec82296a9759e3
Scott <Scotthall1983@hotmail.com>
2019-08-31 08:35:17 +0100

    Created a failing feature test  to confirm planes are no longer in the airport, then added an additional failing Plane test for planes to take off from an airport. Added a take off function to the Plane method in Plane.js.

--- jasmine-standalone-3.5.0/spec/PlaneSpec.js
+++ jasmine-standalone-3.5.0/spec/PlaneSpec.js
@@ -8,11 +8,17 @@ describe('Plane',function(){
   beforeEach(function(){
 
     plane = new Plane();
-    airport = jasmine.createSpyObj('airport',['clearForLanding']);
+    airport = jasmine.createSpyObj('airport',['clearForLanding'],['clearForTakeOff']);
   });
 
   it('can land at an airport', function(){
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+
+  it('can instruct planes to take off', function(){
+    plane.land();
+    plane.takeoff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
   });
 });
