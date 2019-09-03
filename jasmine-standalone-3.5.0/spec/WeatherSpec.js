7ef6e3b65abbfa099c7b13cfcaa94df4f64f8dc7
Scott <Scotthall1983@hotmail.com>
2019-09-03 11:12:38 +0100

    Added an additional failing test that is the opposite of sometimes returning stormy, used Mathrandom to generate if the weather is stormy or not.

--- jasmine-standalone-3.5.0/spec/WeatherSpec.js
+++ jasmine-standalone-3.5.0/spec/WeatherSpec.js
@@ -10,4 +10,8 @@ describe('Weather', function(){
     spyOn(Math, 'random').and.returnValue(1);
     expect(weather.isStormy()).toBeTruthy();
   });
+  it('gives not stormy other times', function(){
+    spyOn(Math, 'random').and.returnValue(0);
+    expect(weather.isStormy()).toBeFalsy();
+  });
 });
